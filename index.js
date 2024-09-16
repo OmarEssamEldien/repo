456f81f9a8933180ecd5880c9e88c0774a457f25
OmarEssamEldien <omaressameldien@gmail.com>
2024-09-16 07:12:55 +0400

    update 1.11

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log('Time', new Date())
 
 console.log('My Name is Omar')
+
+const calc = () => {
+	let rand = Math.random()
+	let num = 23 * rand
+	return Math.round(num)
+}
+
+console.log('I was '+calc())
