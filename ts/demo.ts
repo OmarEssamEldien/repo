20284533c539be0b781229a76d5a5e02c5e2e03d
OmarEssamEldien <omaressameldien@gmail.com>
2024-09-16 13:34:57 +0400

    update 1.2

---
+++ ts/demo.ts
@@ -0,0 +1,7 @@
+document.write("Hello From TypeScript<br>"+new Date())
+
+const sayHi = () => {
+    alert("Hello, "+prompt("What is Your Name?"))
+}
+
+sayHi();
---
+++ ts/scripts/demo.js
@@ -0,0 +1,7 @@
+"use strict";
+document.write("Hello From TypeScript<br>" + new Date());
+const sayHi = () => {
+    alert("Hello, " + prompt("What is Your Name?"));
+};
+sayHi();
+//# sourceMappingURL=demo.js.map
