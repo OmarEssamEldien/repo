b05db932a1f90ccead0bbc9f5266d10ea0c33372
OmarEssamEldien <omaressameldien@gmail.com>
2024-09-16 08:58:34 +0400

    adding timezone

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Time', new Date())
+console.log('Time', new Date(), "+"+(-new Date().getTimezoneOffset()/60))
 
 console.log('My Name is Omar')
 
@@ -6,12 +6,12 @@ const calc = (date) => {
 	let dob = new Date(date)
 	let now = Date.now()
 	let age = now - dob;
-	age = age/1000/3600/24/365.25
+		age = age/1000/3600/24/365.25
 	let years = Math.floor(age)
 	let months = (age-years)*12
 	let days = (months-Math.floor(months))*30
-	months = Math.floor(months)
-	days = Math.round(days)
+		months = Math.floor(months)
+		days = Math.round(days)
 	return years+" Years, "+months+" Months, "+days+" Days"
 }
 
